49079b2ba83f5415078fa9804ede6bb7f8b1bca0
p.m.student <Bolden.Chance02@gmail.com>
2021-03-08 13:28:06 -0600

    Bullseye thing starter

--- OHYES/ViewController.swift
+++ OHYES/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction (title: "", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+        
     }
 
 
